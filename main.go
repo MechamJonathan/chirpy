bdb16403e10a8985ffba23991569cbe64ead5325
MechamJonathan <jonsmec95@gmail.com>
2025-01-24 09:10:37 -0700

    setup database

--- main.go
+++ main.go
@@ -1,28 +1,51 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/MechamJonathan/chirpy/internal/database"
+
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
+	DB             *database.Queries
 	fileserverHits atomic.Int32
 }
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Error")
+	}
+	dbQueries := database.New(db)
+
+	cfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+		DB:             dbQueries,
+	}
+
 	const filepathRoot = "."
 	const port = "8080"
-	apiConfig := &apiConfig{}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", apiConfig.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 
 	mux.HandleFunc("GET /api/healthz", readinessHandler)
-	mux.HandleFunc("POST /api/validate_chirp", apiConfig.handlerChirpsValidate)
+	mux.HandleFunc("POST /api/validate_chirp", cfg.handlerChirpsValidate)
 
-	mux.HandleFunc("GET /admin/metrics", apiConfig.metricsHandler)
-	mux.HandleFunc("POST /admin/reset", apiConfig.resetHandler)
+	mux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", cfg.resetHandler)
 
 	server := &http.Server{
 		Addr:    ":" + port,
