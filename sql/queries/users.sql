6b8bba7df38c5f2bcec05598053645b37c163132
MechamJonathan <jonsmec95@gmail.com>
2025-01-28 04:52:55 -0700

    added authentication

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -12,3 +12,8 @@ RETURNING *;
 -- name: GetUserByEmail :one
 SELECT id, created_at, updated_at, email, hashed_password FROM users
 WHERE email = $1;
+
+-- name: UpdateUser :one
+UPDATE users Set email = $2, hashed_password = $3, updated_at = NOW()
+WHERE id = $1
+RETURNING *;
